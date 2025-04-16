102d014276bdf16a8e1a77768755aacddac1f4df
LilianaAmangoua-vnb <liliana.amangoua@vnb-it.fr>
2025-04-16 14:30:39 +0200

    Tests for avisRoutes (public routes) work

--- tests/routes/avisRoutesPublic.test.js
+++ tests/routes/avisRoutesPublic.test.js
@@ -21,7 +21,7 @@ jest.unstable_mockModule('../../src/controllers/avisController.js', () => ({
 
 let app;
 
-// Création d'une nouvelle app
+// Création d'une nouvelle app express
 beforeEach(async () => {
   jest.resetModules(); 
   jest.clearAllMocks();
@@ -129,6 +129,44 @@ describe("Avis Routes Publiques & Middleware", () => {
     expect(res.body.data).toEqual(mockAvis);
   })
 
+  it("devrait retourner une 400 si l'ID de la chambre n'est pas valide", async ()=> {
+    mockAvisController.getAvisByChambre.mockImplementation((req, res) => {
+      res.status(400).json({
+        status: 'MAUVAISE DEMANDE',
+        message: "L'id de la chambre est invalide"
+      })
+    })
+
+    const avisRoutes = (await import('../../src/routes/avisRoutes.js')).default;
+    app.use('/api/avis', avisRoutes);
+
+    const res = await request(app).get('/api/avis/chambre/n');
+
+    expect(res.status).toBe(400);
+    expect(res.body.status).toBe('MAUVAISE DEMANDE');
+    expect(res.body.message).toBe("L'id de la chambre est invalide");
+
+  })
+
+  it("devrait retourner 404 si aucun avis a été trouvé pour la chambre demandée", async () => {
+    mockAvisController.getAvisByChambre.mockImplementation((req, res) => {
+      res.status(404).json({
+        status: 'RESSOURCE NON TROUVEE',
+        message: "Aucun avis n'a été trouvé pour cette chambre"
+      })
+    })
+
+    const avisRoutes = (await import('../../src/routes/avisRoutes.js')).default;
+    app.use('/api/avis', avisRoutes);
+
+    const res = await request(app).get('/api/avis/chambre/1888');
+
+    expect(res.status).toBe(404);
+    expect(res.body.status).toBe('RESSOURCE NON TROUVEE');
+    expect(res.body.message).toBe("Aucun avis n'a été trouvé pour cette chambre");
+
+  })
+
   it("devrait retourner la moyenne des avis existants", async ()=> {
     const mockMoyenne = 4;
 
@@ -149,4 +187,73 @@ describe("Avis Routes Publiques & Middleware", () => {
     expect(res.body.data).toBe(4);
 
   })
+
+  it("devrait retourner les avis par note", async () => {
+    const mockAvis = [
+      {
+        id_avis: 3,
+        id_reservation: 3,
+        note: 1,
+        commentaire: "Très déçu, service client inexistant.",
+        date_avis: "2025-03-03T08:50:00.000Z"
+      }
+    ];
+
+    mockAvisController.getByNote.mockImplementation((req, res) => {
+        res.status(200).json({
+          status: 'OK',
+          data: mockAvis
+        });
+      }
+    );
+
+    const avisRoutes = (await import('../../src/routes/avisRoutes.js')).default;
+    app.use('/api/avis', avisRoutes);
+
+    const res = await request(app).get('/api/avis/note/1');
+
+    expect(res.status).toBe(200);
+    expect(res.body.status).toBe('OK');
+    expect(res.body.data).toEqual(mockAvis);
+  })
+
+  it("devrait retourner 404 si aucun avis a été trouvé avec cette note", async () => {
+    mockAvisController.getByNote.mockImplementation((req, res) => {
+      res.status(404).json({
+        status: 'RESSOURCE NON TROUVEE',
+        message: "Aucun avis n'a été trouvé pour cette note"
+      })
+    })
+
+    const avisRoutes = (await import('../../src/routes/avisRoutes.js')).default;
+    app.use('/api/avis', avisRoutes);
+
+    const res = await request(app).get('/api/avis/note/2');
+
+    expect(res.status).toBe(404);
+    expect(res.body.status).toBe('RESSOURCE NON TROUVEE');
+    expect(res.body.message).toBe("Aucun avis n'a été trouvé pour cette note");
+
+  })
+
+  it("devrait retourner 400 si la note n'est pas valide", async () => {
+    mockAvisController.getByNote.mockImplementation((req, res) => {
+      res.status(400).json({
+        status: 'MAUVAISE DEMANDE',
+        message: "La note n'est pas valide. Elle doit être comprise entre 1 et 5."
+      })
+    })
+
+    const avisRoutes = (await import('../../src/routes/avisRoutes.js')).default;
+    app.use('/api/avis', avisRoutes);
+
+    const res = await request(app).get('/api/avis/note/10');
+
+    expect(res.status).toBe(400);
+    expect(res.body.status).toBe('MAUVAISE DEMANDE');
+    expect(res.body.message).toBe("La note n'est pas valide. Elle doit être comprise entre 1 et 5.");
+
+
+  })
+
 });
